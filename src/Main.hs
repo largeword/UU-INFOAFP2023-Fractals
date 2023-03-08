ce7479cdd38394c8f40d29f45f590ba1907be9bf
Wescii <71985827+Wescii@users.noreply.github.com>
2023-03-08 14:15:00 +0100

    git conflict test Wes

--- src/Main.hs
+++ src/Main.hs
@@ -9,15 +9,15 @@ main = play (InWindow "Fractals" (500, 500) (0, 0))        -- Or Fullscreen
             black                                          -- Background color
             60                                             -- Frames per second
             World                                          -- Initial state
-            drawHandler                                    -- View function
+            drawHanddler                                    -- View function
             inputHandler                                   -- Event function
             stepHandler                                    -- Step function
 
 
 data World = World
 
-drawHandler :: World -> Picture
-drawHandler _ = Color red $ ThickCircle 15 15
+drawHanddler :: World -> Picture
+drawHanddler _ = Color red $ ThickCircle 15 15
 
 inputHandler :: Event -> World -> World
 inputHandler _ w = w
