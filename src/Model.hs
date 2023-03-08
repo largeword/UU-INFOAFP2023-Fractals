f658fb1322a12a0d2384909813d9f5d6de5bca32
Wescii <71985827+Wescii@users.noreply.github.com>
2023-03-08 15:21:08 +0100

    Basic model of the world

--- src/Model.hs
+++ src/Model.hs
@@ -3,5 +3,22 @@ module Model where
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Interact
 
+-- Separate properties of the screen
+screenWidth :: Int
+screenWidth = 500
 
-data World = MkWorld
+screenHeight :: Int
+screenHeight = 500
+
+-- Data types regarding the representation and calculation of fractals
+data World = MkWorld 
+  { screen         :: [[Point]]
+  , inputEvents    :: [Event]  
+  , zoomScaling    :: Float
+  , translation    :: Int
+  , currentPicture :: Picture  
+}
+
+
+
+-- 
