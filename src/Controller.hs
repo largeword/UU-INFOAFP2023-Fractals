dc6c2c42cc81d2f21a4532432521c5fde549b2d3
Wescii <71985827+Wescii@users.noreply.github.com>
2023-03-08 15:17:21 +0100

    Basic world model and controller blueprint
    Controller.hs contains written comments on what controls need to be handled in the future

--- src/Controller.hs
+++ src/Controller.hs
@@ -10,3 +10,17 @@ inputHandler _ w = w
 
 stepHandler :: Float -> World -> World
 stepHandler _ w = w
+
+
+
+-- KEY MAPPINGS
+
+-- Panning: translation magnitudes (eventually within window app?)
+
+    -- in x direction
+
+    -- in y direction
+
+-- Zoomscaling
+
+-- Later on: loading config document or querying user for input values via console (implement in main.hs in do-block)
--- src/Main.hs
+++ src/Main.hs
@@ -8,12 +8,12 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Interact
 
 main :: IO ()
-main = play (InWindow "Fractals" (500, 500) (0, 0))        -- Or Fullscreen
-            black                                          -- Background color
-            60                                             -- Frames per second
-            MkWorld                                          -- Initial state
-            drawHandlerr                                    -- View function
-            inputHandler                                   -- Event function
-            stepHandler                                    -- Step function
+main = play (InWindow "Fractals" (screenHeight, screenWidth) (0, 0))        -- Or Fullscreen
+            black                                                           -- Background color
+            60                                                              -- Frames per second
+            MkWorld                                                         -- Initial state
+            drawHandlerr                                                    -- View function
+            inputHandler                                                    -- Event function
+            stepHandler                                                     -- Step function
 
 
