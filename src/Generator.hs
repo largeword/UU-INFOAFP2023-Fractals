7c62ad66e18071c3b8e5b2d641aced811db9041c
Wescii <71985827+Wescii@users.noreply.github.com>
2023-03-17 17:24:58 +0100

    Update ZFunction, adjusted functions using ZFunction
    Rewritten ZFunction to accept more parameters and be able to differentiate between different fractals.   Renamed nextZDefault to nextFractalPoint and rewritten the code so that it can be extended to different fractal computations based on the input fractal type. Still using only the Mandelbrot polynomial. Changed getIterations, fixedC, fixedZ and getSequences to work with the new GeneratorData type and ZFunction type signature.
    
    Added some comments with questions about our design.

--- src/Generator.hs
+++ src/Generator.hs
@@ -8,49 +8,72 @@ import Data.Function
 
 import Model
 
-
 -- | A type synonym to define the abstract point calculation function
---   Hopefully, we may be able to allow users to define their own version of this
---   The first argument is the z, the second is the c
-type ZFunction = Point -> Point -> Point
+--   The first argument is the kind of fractal to be generated, the second 
+--   is the degree n of the polynomial z^n in the body of the
+--   function. For our purposes, n is kept constant. The third is the complex paramater c
+--   and the fourth is the starting point z.
 
+type ZFunction = Fractal -> Int -> Point -> Point -> Point    
+-- What do we want as a starting ZFunction?
 
--- | Default implementation of the fractal formula
---   Defines the function z' = z^2 + c
-nextZDefault :: ZFunction
-nextZDefault (zr, zi) (cr, ci) = let (z2r, z2i) = ( zr ** 2 - zi ** 2    -- real
-                                                  , 2 * zr * zi       )  -- imag
-                                  in ( z2r + cr    -- real
-                                     , z2i + ci )  -- imag
+-- | Implementation of the fractal formula per fractal type
+nextFractalPoint :: ZFunction
+
+-- Varies z in the function z' = z^2 + c
+nextFractalPoint MBrot _ (cr, ci) (zr, zi)
+  = let (z2r, z2i) = ( zr ** 2 - zi ** 2          -- real
+                     , 2 * zr * zi       )        -- imag
+    in ( z2r + cr                                 -- real
+       , z2i + ci )                               -- imag
+
+---- Alternative definition for Julia set?
+-- Varies z in the function z' = z^2 + c
+-- nextFractalPoint Julia (zr, zi) (cr, ci) _   -- single-Julia set implementation
+--   = let (z2r, z2i) = ( zr ** 2 - zi ** 2          -- real
+--                      , 2 * zr * zi       )        -- imag
+--     in ( z2r + cr                                 -- real
+--        , z2i + ci )                               -- imag
+
+---- Questions to be answered 
+-- maybe use the pseudocode on wikipedia page to approximate polynomials of higher degrees 
+-- than 2 for multi-Julia sets?
+-- can't we use the same function for MBrot and single-Julia? It's the same calculation,
+-- only different inputs change (different starting points for each iteration of MBrot
+-- and different points p for each Julia set?)
 
 
 -- | Generates a list of length n, defining the iterations
---   according to the ZFunction provided
-getIterations :: ZFunction -> (Point, Point) -> [Point]
-getIterations zf (z, c) = iterate (`zf` c) z
-
-
+--   according to the generation data provided
+getIterations :: GeneratorData -> [Point]
+getIterations genData = let fType = fractalType genData
+                            n     = degree      genData
+                            c     = cplexParam  genData
+                            z     = position    genData
+                        in 
+                          iterate (nextFractalPoint fType n c) z
+                           
 
 ------ Temporary ----------------
 
--- generate iterations of a fixed-z
-fixedC :: GeneratorData -> Point -> [Point]
-fixedC d z = getIterations nextZDefault (z, c)
-  where
-      c = position d
-
 -- generate iterations of a fixed-c
-fixedZ :: Point -> [Point]
-fixedZ c = getIterations nextZDefault (z, c)
-  where
-      z = (0, 0)
+fixedC :: GeneratorData -> Point -> [Point] 
+fixedC genData z = getIterations genData {position = z} 
+                  
+      
+
+-- generate iterations of a fixed-z
+fixedZ :: GeneratorData -> Point -> [Point] -- maybe give a slider to user/input field for changing c
+fixedZ genData c = getIterations genData{cplexParam = c} 
+                
+
 
 ----------------------------------
 
 
 -- | Given a scaled grid, compute the sequences of iterations
 getSequences :: GeneratorData -> Grid Point -> Grid [Point]
-getSequences d grid = gridMap (fixedC d) grid
+getSequences genData grid = gridMap (fixedZ genData) grid
 
 
 -- | Given a sequenced grid and a step treshold n,
