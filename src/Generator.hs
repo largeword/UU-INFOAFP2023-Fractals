2f27cee6454e43beb38ed09c802f00589d5f3715
Wescii <71985827+Wescii@users.noreply.github.com>
2023-03-27 17:03:35 +0200

    Update Generator.hs
    Adjusted (renaming and redefining) fractal generation functions. Fused "fixedC" and "fixedZ" into one function.
    Removed redundant code and comments.

--- src/Generator.hs
+++ src/Generator.hs
@@ -8,72 +8,26 @@ import Data.Function
 
 import Model
 
--- | A type synonym to define the abstract point calculation function
---   The first argument is the kind of fractal to be generated, the second 
---   is the degree n of the polynomial z^n in the body of the
---   function. For our purposes, n is kept constant. The third is the complex paramater c
---   and the fourth is the starting point z.
+-- | Computes a fractal pattern based on fractal meta data and a given point
+generateFractal :: GeneratorData -> Point -> [Point] 
+generateFractal genData pt = case parameter genData of
+  VarZ -> computeFractal genData {position = pt} 
+  VarC -> computeFractal genData {offset   = pt}
+  _    -> error "Other functionalities are not yet defined."    
 
-type ZFunction = Fractal -> Int -> Point -> Point -> Point    
--- What do we want as a starting ZFunction?
-
--- | Implementation of the fractal formula per fractal type
-nextFractalPoint :: ZFunction
-
--- Varies z in the function z' = z^2 + c
-nextFractalPoint MBrot _ (cr, ci) (zr, zi)
-  = let (z2r, z2i) = ( zr ** 2 - zi ** 2          -- real
-                     , 2 * zr * zi       )        -- imag
-    in ( z2r + cr                                 -- real
-       , z2i + ci )                               -- imag
-
----- Alternative definition for Julia set?
--- Varies z in the function z' = z^2 + c
--- nextFractalPoint Julia (zr, zi) (cr, ci) _   -- single-Julia set implementation
---   = let (z2r, z2i) = ( zr ** 2 - zi ** 2          -- real
---                      , 2 * zr * zi       )        -- imag
---     in ( z2r + cr                                 -- real
---        , z2i + ci )                               -- imag
-
----- Questions to be answered 
--- maybe use the pseudocode on wikipedia page to approximate polynomials of higher degrees 
--- than 2 for multi-Julia sets?
--- can't we use the same function for MBrot and single-Julia? It's the same calculation,
--- only different inputs change (different starting points for each iteration of MBrot
--- and different points p for each Julia set?)
-
-
--- | Generates a list of length n, defining the iterations
+-- | Generates the infinite fractal set, defining the iterations
 --   according to the generation data provided
-getIterations :: GeneratorData -> [Point]
-getIterations genData = let fType = fractalType genData
-                            n     = degree      genData
-                            c     = cplexParam  genData
-                            z     = position    genData
-                        in 
-                          iterate (nextFractalPoint fType n c) z
-                           
-
------- Temporary ----------------
-
--- generate iterations of a fixed-c
-fixedC :: GeneratorData -> Point -> [Point] 
-fixedC genData z = getIterations genData {position = z} 
-                  
-      
-
--- generate iterations of a fixed-z
-fixedZ :: GeneratorData -> Point -> [Point] -- maybe give a slider to user/input field for changing c
-fixedZ genData c = getIterations genData{cplexParam = c} 
-                
-
-
-----------------------------------
-
+computeFractal :: GeneratorData -> [Point]
+computeFractal genData = let fracFunc = func     genData 
+                             c        = offset   genData
+                             z        = position genData
+                         in 
+                           iterate (`fracFunc` c) z
+                            
 
 -- | Given a scaled grid, compute the sequences of iterations
 getSequences :: GeneratorData -> Grid Point -> Grid [Point]
-getSequences genData grid = gridMap (fixedZ genData) grid
+getSequences genData grid = gridMap (generateFractal genData) grid
 
 
 -- | Given a sequenced grid and a step treshold n,
