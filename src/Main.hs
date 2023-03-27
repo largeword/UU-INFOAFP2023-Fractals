910a9549befc59db28607fac6661a6e2c5e2b84d
Wescii <71985827+Wescii@users.noreply.github.com>
2023-03-27 16:58:00 +0200

    Update Main.hs
    Adjusted old function names for GenData and integrated the "makeFractalFunction" function in the initialisation code

--- src/Main.hs
+++ src/Main.hs
@@ -17,3 +17,16 @@ main = play (InWindow "Fractals" (screenHeight, screenWidth) (0, 0))        -- O
             stepHandler                                                     -- Step function
 
 
+startWorld :: World
+startWorld = MkWorld
+    [[(fromIntegral $ x - halfScrW, fromIntegral $ y - halfScrH) | x <- [0..screenWidth-1]] | y <- [0..screenHeight-1]]
+    (GenData { position = (0,0), escapeRadius = 100, parameter = VarC, offset = (0,0), func = fracFunc})
+    []
+    1
+    (0, 0)
+    Blank
+    True
+    where
+        fracFunc = makeFractalFunction False 13 
+        -- if 1st arg is True, then absolute value will be taken of z
+        -- second arg represents polynomial degree
