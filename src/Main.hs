ee70a4a6bc923452c8502589ba4cb2d4e987b4e5
De-Signated <31667991+De-Signated@users.noreply.github.com>
2023-04-03 16:02:31 +0200

    Squashed commit of the following:
    commit 080f1f740d1f41e2cef2bedc055ac32132897c1c
    Author: De-Signated <31667991+De-Signated@users.noreply.github.com>
    Date:   Thu Mar 30 15:10:44 2023 +0200
    
        specifiedImplementation finished
    
    commit c5049eeffe04817c92c13aa79c0c45bd76ca557f
    Author: De-Signated <31667991+De-Signated@users.noreply.github.com>
    Date:   Wed Mar 29 16:52:33 2023 +0200
    
        include escapeSteps, include base of specifiedImplementation
    
    commit 2aa885b561307cc3b772f96b135918f1975be340
    Author: De-Signated <31667991+De-Signated@users.noreply.github.com>
    Date:   Wed Mar 29 14:37:59 2023 +0200
    
        start userInput

--- src/Main.hs
+++ src/Main.hs
@@ -3,28 +3,30 @@ module Main where
 import Model
 import Controller
 import View
+import Console
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Interact
+import Debug.Trace
 
 main :: IO ()
-main = play (InWindow "Fractals" (screenHeight, screenWidth) (0, 0))        -- Or Fullscreen
-            black                                                           -- Background color
-            60                                                              -- Frames per second
-            startWorld                                                      -- Initial state
-            drawHandler                                                     -- View function
-            inputHandler                                                    -- Event function
-            stepHandler                                                     -- Step function
+main = do
+    gd <- getGenData
+    let world = startWorld gd
+
+    play (InWindow "Fractals" (screenHeight, screenWidth) (0, 0))        -- Or Fullscreen
+          black                                                          -- Background color
+          60                                                             -- Frames per second
+          world                                                          -- Initial state
+          drawHandler                                                    -- View function
+          inputHandler                                                   -- Event function
+          stepHandler                                                    -- Step function
 
 
-startWorld :: World
-startWorld = MkWorld
+startWorld :: GeneratorData -> World
+startWorld gen = MkWorld
     [[(fromIntegral $ x - halfScrW, fromIntegral $ y - halfScrH) | x <- [0..screenWidth-1]] | y <- [0..screenHeight-1]]
-    (GenData { position = (0,0), escapeRadius = 100, parameter = VarZ, offset = (0,0), func = fracFunc})
+    gen
     (1, (0, 0))
     Blank
     True
-    where
-        fracFunc = makeFractalFunction False 2 
-        -- if 1st arg is True, then absolute value will be taken of z
-        -- second arg represents polynomial degree
