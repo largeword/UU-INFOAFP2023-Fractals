a6f247be0a1e075efb23c51e26d62878d7af378a
De-Signated <31667991+De-Signated@users.noreply.github.com>
2023-04-18 13:11:31 +0200

    fixed colour

--- src/ViewAcc.hs
+++ src/ViewAcc.hs
@@ -27,15 +27,15 @@ drawHandler (MkWorld _ _ _ p _) = p
 --   Input parameters are (unscaled) grid of points and grid of colours
 --   By zipping them together we can easily map pointToPicture over them
 --   Then the result is condensed into a single picture
-draw :: Grid (Point, Color) -> Picture
+draw :: Grid (Point, ColorAcc) -> Picture
 draw pointAndCol = let pics = gridMap pointToPicture pointAndCol
                     in Pictures . Prelude.map Pictures $ pics
 
 -- | Very simple: generate a pixel point, and give it the right colour and translation
 --   Because the translation is wrt the screen coordinates as opposed to view coordinates,
 --   these need to be the unscaled versions.
-pointToPicture :: (Point, Color) -> Picture
-pointToPicture ((x, y), c) = let (r, g, b, a) = rgbaOfColor c
+pointToPicture :: (Point, ColorAcc) -> Picture
+pointToPicture ((x, y), c) = let (r, g, b, a) = c
                                  c'           = makeColor r g b a
                               in Translate x y $ Color c' $ Circle 1
 
